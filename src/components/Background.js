5ee4df9eda2993f8af4f7662d5340bca1112c8dc
Sahil <sahil@Sahils-MacBook-Pro.local>
2023-06-08 16:15:54 +0530

    view my resume works now

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,7 +6,7 @@ const Background = () => {
                 <p>I am a undergrad studing in <span>Vasantdada Patil Pratishthan's College of Engineering & Visual Arts for Bachelors in Engineering</span>, Computer Engineering</p>
                 <p>As a web developer, Lenjoy bridging the gap between engineering and design-combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging pixel-perfect user experiences.</p>
                 <p><span>When I'm not in front of a computer screen</span>, I'm probably reading books, travelling or tweeting</p>
-                <button>View my resume</button>
+                <a href="https://drive.google.com/file/d/1Webp2Wufd8qGb5XJCg5Q0nGEd0-18y-u/view?usp=sharing" target="_blank">View my resume</a>
             </div>
         </div>
     )
