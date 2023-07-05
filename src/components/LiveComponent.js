aa00bb6535dce3f23d1368ce965a14fc16efaace
Sahil <sahil@Sahils-MacBook-Pro.local>
2023-07-06 01:22:38 +0530

    dynamic comments added

--- src/components/LiveComponent.js
+++ src/components/LiveComponent.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react'
 const LiveComponent = () => {
     const [lcData, setLCData] = useState({})
     const getData = async() =>{
-        const data = await fetch("https://leetcode-stats-api.herokuapp.com/sahil643");
+        const data = await fetch("https://leetcode-stats-api.herokuapp.com/0xItachi");
         const json = await data.json();
         // console.log(json)
         setLCData(json)
@@ -19,10 +19,10 @@ const LiveComponent = () => {
         <p><span>Username:</span> sahil643</p>
         <p><span>Ranking:</span> {lcData.ranking}</p>
         <p><span>Total Solved:</span> {lcData.totalSolved}</p>
-        <p><span>Total Easy Solved:</span> {lcData.easySolved} / {lcData.totalEasy}</p>
+        {/* <p><span>Total Easy Solved:</span> {lcData.easySolved} / {lcData.totalEasy}</p>
         <p><span>Total Medium Solved:</span> {lcData.mediumSolved} / {lcData.totalMedium}</p>
         <p><span>Total Hard Solved:</span> {lcData.hardSolved} / {lcData.totalHard}</p>
-        <p><span>Acceptance Rate:</span> {lcData.acceptanceRate}</p>
+        <p><span>Acceptance Rate:</span> {lcData.acceptanceRate}</p> */}
       </div>
     </div>
   );
