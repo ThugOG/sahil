c8ab4d5ecfb32f53ae690148b69c10b7fd1e5900
Sahil <sahil@Sahils-MacBook-Pro.local>
2023-07-06 01:34:14 +0530

    dynamic comments added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,27 @@
+import { useState } from "react"
 import { projects } from "../utils/extras"
 import ProjectCard from "./ProjectCard"
 const Projects = () => {
+    const [showAll, setShowAll] = useState(false)
+    const handleClick = () => {
+        setShowAll(!showAll)
+    }
     return(
         <div className="projects">
             <div className="pj-left"><p>Projects</p></div>
             <div className="pj-right">
                 {
                    projects.map((single, index)=>{
-                        return <ProjectCard key={index} {...single}/>
+                        if(showAll)
+                            return <ProjectCard key={index} {...single}/>
+                        else{
+                            if(index <= 2){
+                                return <ProjectCard key={index} {...single}/>
+                            }
+                        }
                    }) 
                 }
-                <p>Check my Other Project on github</p>
+                <button className="btn-view-more" onClick={handleClick}>Show More</button>
             </div>
         </div>
     )
