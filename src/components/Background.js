d75eb13fcfc5dadc023675001d14a66d28da59c4
0xThug <sahilgthakur1@gmail.com>
2025-03-11 20:50:36 +0530

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,7 +6,7 @@ const Background = () => {
                 <p>I am a undergrad studing in <span>Vasantdada Patil Pratishthan's College of Engineering & Visual Arts for Bachelors in Engineering</span>, Computer Engineering</p>
                 <p>As a web developer, I enjoy bridging the gap between engineering and design-combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging pixel-perfect user experiences.</p>
                 <p><span>When I'm not in front of a computer screen</span>, I'm probably reading books, travelling, tweeting, wandering on Discord or catching up with One Piece.</p>
-                <a href="https://drive.google.com/file/d/1xU5C0Nge5LdYapSgKEjG60_AR28apD_k/view?usp=sharing" target="_blank">View my resume</a>
+                <a href="https://drive.google.com/file/d/1Bjks8LqvbugfLNYo-yHtEc7vB-93OQH6/view?usp=sharing" target="_blank">View my resume</a>
             </div>
         </div>
     )
